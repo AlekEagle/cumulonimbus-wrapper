ed27164961b1066659da1831d7c7117d39924d0b
Alek Evans <github@alekeagle.com>
2022-01-14 09:23:38 -0700

    idfk

--- src/RequestHandler.ts
+++ src/RequestHandler.ts
@@ -15,42 +15,18 @@ export default async function call<T>(
   url: string,
   options: RequestInit = { headers: {} }
 ) {
-  let opts = Object.assign(Object.create(null), options);
-  if (!opts.headers) opts.headers = {};
-  (opts.headers as any)[
-    'User-Agent'
-  ] = `Cumulonimbus-Wrapper: ${Cumulonimbus.VERSION}`;
-  let res: Response = await fetchFun(Cumulonimbus.BASE_URL + url, opts);
-
-  if (res.status === 413) {
-    throw new Cumulonimbus.ResponseError({
-      code: 'BODY_TOO_LARGE_ERROR',
-      message: 'Request Body Too Large',
-      ratelimit: {
-        maxRequests: Number(res.headers.get('X-RateLimit-Limit')),
-        remainingRequests: Number(res.headers.get('X-RateLimit-Remaining')),
-        resetsAt: Number(res.headers.get('X-RateLimit-Reset'))
-      }
-    });
-  }
-
   try {
-    let json: T = await res.json();
-    if (res.ok) {
-      return {
-        res,
-        payload: {
-          ...json,
-          ratelimit: {
-            maxRequests: Number(res.headers.get('X-RateLimit-Limit')),
-            remainingRequests: Number(res.headers.get('X-RateLimit-Remaining')),
-            resetsAt: Number(res.headers.get('X-RateLimit-Reset'))
-          }
-        }
-      };
-    } else {
+    let opts = Object.assign(Object.create(null), options);
+    if (!opts.headers) opts.headers = {};
+    (opts.headers as any)[
+      'User-Agent'
+    ] = `Cumulonimbus-Wrapper: ${Cumulonimbus.VERSION}`;
+    let res: Response = await fetchFun(Cumulonimbus.BASE_URL + url, opts);
+
+    if (res.status === 413) {
       throw new Cumulonimbus.ResponseError({
-        ...(json as unknown as Cumulonimbus.Data.Error),
+        code: 'BODY_TOO_LARGE_ERROR',
+        message: 'Request Body Too Large',
         ratelimit: {
           maxRequests: Number(res.headers.get('X-RateLimit-Limit')),
           remainingRequests: Number(res.headers.get('X-RateLimit-Remaining')),
@@ -58,17 +34,48 @@ export default async function call<T>(
         }
       });
     }
+
+    try {
+      let json: T = await res.json();
+      if (res.ok) {
+        return {
+          res,
+          payload: {
+            ...json,
+            ratelimit: {
+              maxRequests: Number(res.headers.get('X-RateLimit-Limit')),
+              remainingRequests: Number(
+                res.headers.get('X-RateLimit-Remaining')
+              ),
+              resetsAt: Number(res.headers.get('X-RateLimit-Reset'))
+            }
+          }
+        };
+      } else {
+        throw new Cumulonimbus.ResponseError({
+          ...(json as unknown as Cumulonimbus.Data.Error),
+          ratelimit: {
+            maxRequests: Number(res.headers.get('X-RateLimit-Limit')),
+            remainingRequests: Number(res.headers.get('X-RateLimit-Remaining')),
+            resetsAt: Number(res.headers.get('X-RateLimit-Reset'))
+          }
+        });
+      }
+    } catch (error) {
+      if (error instanceof Cumulonimbus.ResponseError) throw error;
+      else
+        throw new Cumulonimbus.ResponseError({
+          code: 'GENERIC_ERROR',
+          message: null,
+          ratelimit: {
+            maxRequests: Number(res.headers.get('X-RateLimit-Limit')),
+            remainingRequests: Number(res.headers.get('X-RateLimit-Remaining')),
+            resetsAt: Number(res.headers.get('X-RateLimit-Reset'))
+          }
+        });
+    }
   } catch (error) {
-    if (error instanceof Cumulonimbus.ResponseError) throw error;
-    else
-      throw new Cumulonimbus.ResponseError({
-        code: 'GENERIC_ERROR',
-        message: null,
-        ratelimit: {
-          maxRequests: Number(res.headers.get('X-RateLimit-Limit')),
-          remainingRequests: Number(res.headers.get('X-RateLimit-Remaining')),
-          resetsAt: Number(res.headers.get('X-RateLimit-Reset'))
-        }
-      });
+    console.error(error);
+    throw error;
   }
 }
