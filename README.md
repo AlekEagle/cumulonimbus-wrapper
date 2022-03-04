919be8194af5b0214d8a2054072aa261879d12f0
Alek Evans <gpg@alekeagle.com>
2022-03-03 19:10:08 -0700

    remove node fetch from dependencies
    node fetch is still a dependency server side, but removed for compatibility issues in vue

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cumulonimbus Wrapper
+
+## Note
+
+If you plan on using this in nodejs you must install `node-fetch` otherwise you will get an error.
--- src/Cumulonimbus.ts
+++ src/Cumulonimbus.ts
@@ -1,7 +1,7 @@
 export namespace Cumulonimbus {
   export const BASE_URL = 'https://alekeagle.me/api';
 
-  export const VERSION = 'v1.0.16';
+  export const VERSION = 'v1.0.17';
 
   export interface RateLimitData {
     maxRequests: number;
