d66d02aa318185b2bdc5e134e5e501e2d42835f5
Alek Evans <gpg@alekeagle.com>
2022-03-03 19:20:50 -0700

    undo stupid

--- src/Cumulonimbus.ts
+++ src/Cumulonimbus.ts
@@ -1,7 +1,7 @@
 export namespace Cumulonimbus {
   export const BASE_URL = 'https://alekeagle.me/api';
 
-  export const VERSION = 'v1.0.17';
+  export const VERSION = 'v1.0.18';
 
   export interface RateLimitData {
     maxRequests: number;
