feac76483963b7aa9765d1425570603c252cfc11
Alek Evans <alekeagle@alekeagle.com>
2022-05-10 14:58:08 +0000

    whoops

--- src/Cumulonimbus.ts
+++ src/Cumulonimbus.ts
@@ -1,7 +1,7 @@
 export namespace Cumulonimbus {
   export const BASE_URL = 'https://alekeagle.me/api';
 
-  export const VERSION = 'v1.0.19';
+  export const VERSION = 'v1.0.21';
 
   export interface RateLimitData {
     maxRequests: number;
