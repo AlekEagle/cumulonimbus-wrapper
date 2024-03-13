301035290761837758ed804741ece8b471d78a83
Alek Evans <github@alekeagle.com>
2024-03-12 16:43:21 -0800

    v4.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Hard-code the version number, because it's not worth the effort to automate it
-const version = '4.0.4';
+const version = '4.0.5';
 
 // deep merge two objects without overwriting existing properties
 function merge(obj1: any, obj2: any) {
